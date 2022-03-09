966f7e3c506d6f5160711c49a3b7ad2520b1abc4
Nathan Hui <ntlhui@gmail.com>
2022-03-08 19:59:30 -0800

    Added Caffeinate install

--- auto_install.ps1
+++ auto_install.ps1
@@ -14,3 +14,15 @@ python3.10 -m venv .venv
 python -m pip install --upgrade pip
 python -m pip install --upgrade .
 explorer .venv\Scripts
+if (Get-Command $cmdName -errorAction SilentlyContinue)
+{
+    "$cmdName exists"
+}
+else
+{
+	$File = $(New-TemporaryFile ).FullName + ".exe"
+	Invoke-WebRequest -Uri "https://download.visualstudio.microsoft.com/download/pr/1eb43f77-61af-40b0-8a5a-6165724dca60/f12aac6d4a907b4d54f5d41317aae0f7/dotnet-sdk-6.0.201-win-x64.exe" -OutFile $File
+	$File
+	dotnet tool install --global Clcrutch.Caffeinate --version 0.1.4-alpha
+
+}
