0e8ac57bd20fcff265fec7bb0aea1cdb455d5d69
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2022-03-11 07:46:33 -0500

    refactor ui into own class

---
+++ MangroveDataManagement/main_window.py
@@ -0,0 +1,110 @@
+from tkinter import Tk, Label, StringVar, OptionMenu, Entry, BooleanVar, Checkbutton, Button, messagebox
+from tkcalendar import DateEntry
+from typing import Any, Callable, Dict, List
+
+FONT_NAME = 'Segoe UI'
+FONT_SIZE = 18
+FONT = (FONT_NAME, FONT_SIZE)
+
+class MainWindow(Tk):
+    def __init__(self, config: Dict[str, Any], removable_drives: List[str], fixed_drives: List[str], copy_clicked_callback: Callable[[Dict[str, Any]], None]):
+        Tk.__init__(self)
+        self._config = config
+        self._copy_clicked_callback = copy_clicked_callback
+
+        self.title('Data Organizer')
+        self.config(bg='black')
+        self.grid_columnconfigure(1, weight=1)
+
+        _, _, self._source_drive_variable = self._add_dropdown('Source Drive', 0, removable_drives, default=self._get_value_or_default(config, 'source_drive'))
+        _, _, self._target_drive_variable = self._add_dropdown('Target Drive', 1, fixed_drives, default=self._get_value_or_default(config, 'target_drive'))
+        _, self._aircraft_entry = self._add_entry('Aircraft', 2, default=self._get_value_or_default(config, 'aircraft'))
+        _, self._country_entry = self._add_entry('Country', 3, default=self._get_value_or_default(config, 'country'))
+        _, self._region_entry = self._add_entry('Region', 4, default=self._get_value_or_default(config, 'region'))
+        _, self._date_entry = self._add_date('Flight Date', 5)
+        _, self._site_entry = self._add_entry('Site', 6)
+        _, self._flight_entry = self._add_entry('Flight', 7)
+        _, self._delete_variable = self._add_checkbox('Delete Files', 8, default=self._get_value_or_default(config, 'delete_files', default=True))
+        
+        copy_button = Button(self, text='Copy', command=self._copy_command)
+        copy_button.grid(row=9, column=1, pady=(10, 10))
+        copy_button.config(bg='black', fg='white', font=FONT)
+
+    def _fix_input(self, input: str):
+        return input.lower().replace(' ', '-')
+
+    def _copy_command(self):
+        self._config['source_drive'] = self._source_drive_variable.get()
+        self._config['target_drive'] = self._target_drive_variable.get()
+        self._config['aircraft'] = self._aircraft_entry.get()
+        self._config['country'] = self._fix_input(self._country_entry.get())
+        self._config['region'] = self._fix_input(self._region_entry.get())
+        self._config['date'] = self._date_entry.get_date()
+        self._config['site'] = self._fix_input(self._site_entry.get())
+        self._config['flight'] = self._fix_input(self._flight_entry.get())
+        self._config['delete_files'] = self._delete_variable.get()
+
+        if not self._config['aircraft'] or not self._config['country'] or not self._config['region'] or not self._config['date'] or not self._config['site'] or not self._config['flight']:
+            messagebox.showerror(title='Missing data', message='Please ensure all fields have been filled out and try again.')
+            return
+
+        self._copy_clicked_callback(self._config)
+
+    def get_value_or_default(self, config: Dict[str, Any], key: str, default=None):
+        return config[key] if key in config else default
+
+    def _add_dropdown(self, label: str, row: int, data: List[str], default=None):
+        label = Label(self, text=label)
+        label.grid(row=row, column=0)
+        label.config(bg='black', fg='white', font=FONT)
+
+        variable = StringVar()
+        if default and default in data:
+            variable.set(default)
+        elif data:
+            variable.set(data[0])
+
+        dropdown = OptionMenu(self, variable, *data)
+        dropdown.grid(row=row, column=1, sticky='we', pady=(10, 10))
+        dropdown.config(bg='black', fg='white', font=FONT)
+
+        return label, dropdown, variable
+
+    def _add_entry(self, label: str, row: int, default=None):
+        label = Label(self, text=label)
+        label.grid(row=row, column=0)
+        label.config(bg='black', fg='white', font=FONT)
+
+        entry = Entry(self)
+        entry.grid(row=row, column=1, sticky='we', pady=(10, 10))
+        entry.config(bg='black', fg='white', insertbackground='white', font=FONT)
+
+        if default:
+            entry.insert(0, default)
+
+        return label, entry
+
+    def _add_date(self, label: str, row: int, default=None):
+        label = Label(self, text=label)
+        label.grid(row=row, column=0)
+        label.config(bg='black', fg='white', font=FONT)
+
+        date = DateEntry(self, selectmode='day')
+        date.grid(row=row, column=1, sticky='we', pady=(10, 10))
+        date.config(font=FONT)
+
+        if default:
+            date.set_date(default)
+
+        return label, date
+
+    def _add_checkbox(self, label: str, row:int, default=None):
+        variable = BooleanVar()
+        if default:
+            variable.set(default)
+
+        checkbox = Checkbutton(self, text=label,variable=variable, onvalue=True, offvalue=False)
+        checkbox.grid(row=row, column=0, pady=(10, 10))
+        checkbox.config(bg='black', fg='white', selectcolor='black', activebackground='black', activeforeground='white', font=FONT)
+
+        return checkbox, variable
--- MangroveDataManagement/manager.py
+++ MangroveDataManagement/manager.py
@@ -1,9 +1,6 @@
 from typing import Any, Dict, List
 import win32api
 import win32file
-import tkinter
-import tkinter.ttk
-import tkcalendar
 import os
 import pathlib
 import pickle
@@ -11,15 +8,13 @@ from tkinter import messagebox
 from glob import glob
 import shutil
 import wmi
-from .caf import Caffine
 from shutil import which
 from subprocess import Popen
 import time
 import math
 
-FONT_NAME = 'Segoe UI'
-FONT_SIZE = 18
-FONT = (FONT_NAME, FONT_SIZE)
+from .caf import Caffine
+from .main_window import MainWindow
 
 def has_files(drive: str):
     return os.path.exists(os.path.join(drive, 'DCIM'))
@@ -37,62 +32,6 @@ def get_fixed_drives():
     logical_disks = [F'{logical_disk2partition_map[p.DeviceID].DeviceID}\\ ({d.Model})' for d, p in disk_drive2disk_partition_filter if p.DeviceID in logical_disk2partition_map]
     
     return [F'Desktop ({os.path.join(os.path.expanduser("~"), "Desktop")})'] + logical_disks
-
-def add_dropdown(root: tkinter.Tk, label: str, row: int, data: List[str], default=None):
-    label = tkinter.Label(root, text=label)
-    label.grid(row=row, column=0)
-    label.config(bg='black', fg='white', font=FONT)
-
-    variable = tkinter.StringVar()
-    if default and default in data:
-        variable.set(default)
-    elif data:
-        variable.set(data[0])
-
-    dropdown = tkinter.OptionMenu(root, variable, *data)
-    dropdown.grid(row=row, column=1, sticky='we', pady=(10, 10))
-    dropdown.config(bg='black', fg='white', font=FONT)
-
-    return label, dropdown, variable
-
-def add_entry(root: tkinter.Tk, label: str, row: int, default=None):
-    label = tkinter.Label(root, text=label)
-    label.grid(row=row, column=0)
-    label.config(bg='black', fg='white', font=FONT)
-
-    entry = tkinter.Entry(root)
-    entry.grid(row=row, column=1, sticky='we', pady=(10, 10))
-    entry.config(bg='black', fg='white', insertbackground='white', font=FONT)
-
-    if default:
-        entry.insert(0, default)
-
-    return label, entry
-
-def add_date(root: tkinter.Tk, label: str, row: int, default=None):
-    label = tkinter.Label(root, text=label)
-    label.grid(row=row, column=0)
-    label.config(bg='black', fg='white', font=FONT)
-
-    date = tkcalendar.DateEntry(root, selectmode='day')
-    date.grid(row=row, column=1, sticky='we', pady=(10, 10))
-    date.config(font=FONT)
-
-    if default:
-        date.set_date(default)
-
-    return label, date
-
-def add_checkbox(root: tkinter.Tk, label: str, row:int, default=None):
-    variable = tkinter.BooleanVar()
-    if default:
-        variable.set(default)
-
-    checkbox = tkinter.Checkbutton(root, text=label,variable=variable, onvalue=True, offvalue=False)
-    checkbox.grid(row=row, column=0, pady=(10, 10))
-    checkbox.config(bg='black', fg='white', selectcolor='black', activebackground='black', activeforeground='white', font=FONT)
-
-    return checkbox, variable
 
 def create_directories(path: str):
     path_obj = pathlib.Path(path)
@@ -116,20 +55,20 @@ def load_config() -> Dict[str, Any]:
     
     return {}
 
-def copy(root: tkinter.Tk, config: Dict[str, Any]):
+def copy(config: Dict[str, Any]):
     caf = Caffine()
     request_id = caf.request()
 
-    progress_win = tkinter.Toplevel()
-    progress_win.geometry('500x100')
-    progress_win.config(bg='black')
+    # progress_win = tkinter.Toplevel()
+    # progress_win.geometry('500x100')
+    # progress_win.config(bg='black')
 
-    progress = tkinter.ttk.Progressbar(progress_win)
-    progress.pack(fill='x', ipady=10, expand=True)
+    # progress = tkinter.ttk.Progressbar(progress_win)
+    # progress.pack(fill='x', ipady=10, expand=True)
 
-    remaining_time = tkinter.Label(progress_win)
-    remaining_time.pack(fill='x')
-    remaining_time.config(bg='black', fg='white', font=FONT)
+    # remaining_time = tkinter.Label(progress_win)
+    # remaining_time.pack(fill='x')
+    # remaining_time.config(bg='black', fg='white', font=FONT)
 
     base_target_path, _ = config['target_drive'].split(' ')
     if base_target_path == 'Desktop':
@@ -149,7 +88,7 @@ def copy(root: tkinter.Tk, config: Dict[str, Any]):
         '**/*'
     ))
 
-    progress.config(length=len(source_files))
+    # progress.config(length=len(source_files))
 
     target_files = [os.path.join(copy_path, os.path.basename(f)) for f in source_files]
     existing_target_files = [f for f in target_files if os.path.exists(f)]
@@ -163,18 +102,18 @@ def copy(root: tkinter.Tk, config: Dict[str, Any]):
         caf.release(request_id)
         return
 
-    start = time.perf_counter()
-    for i, file in enumerate(source_files):
-        file_name = os.path.basename(file)
-        shutil.copyfile(file, os.path.join(copy_path, file_name))
-        elapsed = time.perf_counter() - start
-        time_per_item = elapsed / (float(i) + 1.0)
-        total_time = time_per_item * len(source_files)
-        time_remaining = total_time - time_per_item
-        minutes_remaing = math.floor(time_remaining / 60)
-        seconds_remaining = time_remaining - minutes_remaing * 60
-        progress['value'] = (float(i) + 1.0) * 100.0 / float(len(source_files))
-        remaining_time.config(text=F'{minutes_remaing}:{round(seconds_remaining * 1000) / 1000} remaining')
+    # start = time.perf_counter()
+    # for i, file in enumerate(source_files):
+    #     file_name = os.path.basename(file)
+    #     shutil.copyfile(file, os.path.join(copy_path, file_name))
+    #     elapsed = time.perf_counter() - start
+    #     time_per_item = elapsed / (float(i) + 1.0)
+    #     total_time = time_per_item * len(source_files)
+    #     time_remaining = total_time - time_per_item
+    #     minutes_remaing = math.floor(time_remaining / 60)
+    #     seconds_remaining = time_remaining - minutes_remaing * 60
+        # progress['value'] = (float(i) + 1.0) * 100.0 / float(len(source_files))
+        # remaining_time.config(text=F'{minutes_remaing}:{round(seconds_remaining * 1000) / 1000} remaining')
 
     if config['delete_files']:
         folder_to_delete = os.path.join(
@@ -192,17 +131,11 @@ def copy(root: tkinter.Tk, config: Dict[str, Any]):
             Popen([file_path, folder_to_delete])
 
     caf.release(request_id)
-    progress_win.destroy()
+    # progress_win.destroy()
 
     # Launch explorer.
     file_path = which('explorer')
     Popen([file_path, copy_path])
-
-def get_value_or_default(config: Dict[str, Any], key: str, default=None):
-    return config[key] if key in config else default
-
-def fix_input(input: str):
-    return input.lower().replace(' ', '-')
 
 def main():
     # Only run if a removable drive is attached.
@@ -211,44 +144,12 @@ def main():
         messagebox.showerror(title='No removable drives attached', message='Check that your SD card is plugged in and try again.')
         return
 
-    root = tkinter.Tk()
-    root.title('Data Organizer')
-    root.config(bg='black')
-    root.grid_columnconfigure(1, weight=1)
-
     config = load_config()
+    root = MainWindow(config, removable_drives, get_fixed_drives(), copy_callback)
 
-    source_drive_label, source_drive_dropdown, source_drive_variable = add_dropdown(root, 'Source Drive', 0, removable_drives, default=get_value_or_default(config, 'source_drive'))
-    target_drive_label, target_drive_dropdown, target_drive_variable = add_dropdown(root, 'Target Drive', 1, get_fixed_drives(), default=get_value_or_default(config, 'target_drive'))
-    aircraft_label, aircraft_entry = add_entry(root, 'Aircraft', 2, default=get_value_or_default(config, 'aircraft'))
-    country_label, country_entry = add_entry(root, 'Country', 3, default=get_value_or_default(config, 'country'))
-    region_label, region_entry = add_entry(root, 'Region', 4, default=get_value_or_default(config, 'region'))
-    date_label, date_entry = add_date(root, 'Flight Date', 5)
-    site_label, site_entry = add_entry(root, 'Site', 6)
-    flight_label, flight_entry = add_entry(root, 'Flight', 7)
-    delete_checkbox, delete_variable = add_checkbox(root, 'Delete Files', 8, default=get_value_or_default(config, 'delete_files', default=True))
-
-    def copy_command():
-        config['source_drive'] = source_drive_variable.get()
-        config['target_drive'] = target_drive_variable.get()
-        config['country'] = fix_input(country_entry.get())
-        config['region'] = fix_input(region_entry.get())
-        config['date'] = date_entry.get_date()
-        config['site'] = fix_input(site_entry.get())
-        config['flight'] = fix_input(flight_entry.get())
-        config['aircraft'] = aircraft_entry.get()
-        config['delete_files'] = delete_variable.get()
-
-        if not config['aircraft'] or not config['country'] or not config['region'] or not config['date'] or not config['site'] or not config['flight']:
-            messagebox.showerror(title='Missing data', message='Please ensure all fields have been filled out and try again.')
-            return
-
+    def copy_callback(config):
         save_config(config)
         copy(root, config)
-
-    copy_button = tkinter.Button(root, text='Copy', command=copy_command)
-    copy_button.grid(row=9, column=1, pady=(10, 10))
-    copy_button.config(bg='black', fg='white', font=FONT)
 
     root.mainloop()
 
